4edcb960411d44f613c868238c6e833efe1ffaf0
AziSudarya1 <azisudarya98@gmail.com>
2025-04-15 22:04:29 +0700

    chore: replace then catch to async await

--- frontend/src/atoms/tokenAtoms.js
+++
@@ -1,6 +0,0 @@
-import { atom } from 'jotai';
-import { createStore } from 'jotai';
-
-export const store = createStore();
-export const accessTokenAtom = atom(null);
-export const expiredAtom = atom(null);
